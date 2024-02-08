aaea05ca2a92381e4039b15d43a65c1039d8969b
Arijit Paria <arijitparia2020@gmail.com>
2024-02-08 15:11:34 +0530

    Complete tictactoe logic

--- TikTakToe.java
+++ TikTakToe.java
@@ -5,23 +5,32 @@ import java.awt.event.*;
 public class TikTakToe {
 
     static boolean isOdd = true;
+    static JButton[] buttons = new JButton[9];
 
     public static void main(String[] args) {
-        // my listner class
-        class MyListner implements ActionListener {
+        class MyListener implements ActionListener {
             public void actionPerformed(ActionEvent ae) {
-                System.out.println("ButtonClicked");
                 JButton b = (JButton) ae.getSource();
+                if (!b.getText().equals("")) {
+                    return; // If the button already has a symbol, do nothing
+                }
                 if (isOdd) {
-                    isOdd = false;
                     b.setText("X");
-
                 } else {
-                    isOdd = true;
                     b.setText("O");
                 }
+                isOdd = !isOdd;
                 b.setEnabled(false);
-
+                if (checkWin("X")) {
+                    JOptionPane.showMessageDialog(null, "Player X wins!");
+                    resetBoard();
+                } else if (checkWin("O")) {
+                    JOptionPane.showMessageDialog(null, "Player O wins!");
+                    resetBoard();
+                } else if (isBoardFull()) {
+                    JOptionPane.showMessageDialog(null, "It's a draw!");
+                    resetBoard();
+                }
             }
         }
 
@@ -31,20 +40,60 @@ public class TikTakToe {
         f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        // grid layout
         GridLayout gl = new GridLayout(3, 3, 4, 4);
         JPanel jp = new JPanel(gl);
 
         for (int i = 0; i < 9; i++) {
             JButton bt = new JButton("");
             bt.setFont(new Font("Serif", i, 30));
-            bt.addActionListener(new MyListner());
+            bt.addActionListener(new MyListener());
             jp.add(bt);
+            buttons[i] = bt; // store buttons in an array for easier access
         }
 
         f.add(jp);
 
         f.setVisible(true);
 
+    }
+
+    static boolean checkWin(String symbol) {
+        // Check rows
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i * 3].getText().equals(symbol) && buttons[i * 3 + 1].getText().equals(symbol) && buttons[i * 3 + 2].getText().equals(symbol)) {
+                return true;
+            }
+        }
+        // Check columns
+        for (int i = 0; i < 3; i++) {
+            if (buttons[i].getText().equals(symbol) && buttons[i + 3].getText().equals(symbol) && buttons[i + 6].getText().equals(symbol)) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if (buttons[0].getText().equals(symbol) && buttons[4].getText().equals(symbol) && buttons[8].getText().equals(symbol)) {
+            return true;
+        }
+        if (buttons[2].getText().equals(symbol) && buttons[4].getText().equals(symbol) && buttons[6].getText().equals(symbol)) {
+            return true;
+        }
+        return false;
+    }
+
+    static boolean isBoardFull() {
+        for (JButton button : buttons) {
+            if (button.getText().equals("")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void resetBoard() {
+        for (JButton button : buttons) {
+            button.setText("");
+            button.setEnabled(true);
+        }
+        isOdd = true; // Reset the turn to X
     }
 }
