274d94241badcbc63c11e980d463cbe0008f83e8
Arijit Paria <arijitparia2020@gmail.com>
2024-02-15 15:59:00 +0530

    save functionality ✅

--- MenubarIntro.java
+++ MenubarIntro.java
@@ -1,8 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
 public class MenubarIntro {
+    static JTextArea textarea = new JTextArea();
+
     public static void main(String... rk) {
+        // action listner
+        class MyListener implements ActionListener {
+            public void actionPerformed(ActionEvent ae) {
+                String command = ae.getActionCommand();
+                if (command.equals("Save")) {
+                    saveFile();
+                }
+                if(command.equals("Open")){
+                    OpenFile();
+                }
+            }
+        }
+
         JFrame frame = new JFrame("Menubar Introduction");
 
         frame.setSize(500, 600);
@@ -12,7 +29,7 @@ public class MenubarIntro {
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
         // add text area
-        JTextArea textarea = new JTextArea();
+        // JTextArea textarea = new JTextArea();
         JScrollPane scrollpane = new JScrollPane(textarea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                 ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         frame.add(scrollpane);
@@ -35,7 +52,9 @@ public class MenubarIntro {
                 menu.add(submenu2);
                 menu1.add(menu);
             } else {
-                menu1.add(new JMenuItem(item));
+                JMenuItem menuitem = new JMenuItem(item);
+                menuitem.addActionListener(new MyListener());
+                menu1.add(menuitem);
             }
         }
         menu1.insertSeparator(2); // adds a separator between the menuitems
@@ -67,6 +86,41 @@ public class MenubarIntro {
 
         frame.setJMenuBar(menubar);
         frame.setVisible(true);
+
+    }
+
+    // save file method
+    static void saveFile() {
+        // save the file to the path provided using file output stream
+        JFileChooser choosefile = new JFileChooser();
+        int result = choosefile.showSaveDialog(null);
+        if (result == JFileChooser.APPROVE_OPTION) {
+            File file = choosefile.getSelectedFile();
+            try {
+                String text = textarea.getText();
+                FileOutputStream fos = new FileOutputStream(file.getPath());
+                byte[] b = text.getBytes();
+                fos.write(b);
+                fos.close();
+                JOptionPane.showMessageDialog(null, "File saved successfully");
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+
+    }
+
+    // open file method
+    static void OpenFile(){
+        JFileChooser choosefile = new JFileChooser();
+        int result = choosefile.showOpenDialog(null);
+        
+        if(result == JFileChooser.APPROVE_OPTION){
+            File file = choosefile.getSelectedFile();
+            System.out.println(file);
+        }
+
+        
     }
 
 }
