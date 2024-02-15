b25e020ca19921ebe361a086bb588ae8db7b57bd
Arijit Paria <arijitparia2020@gmail.com>
2024-02-15 15:10:51 +0530

    Added text area notepad style

--- MenubarIntro.java
+++ MenubarIntro.java
@@ -8,12 +8,19 @@ public class MenubarIntro {
         frame.setSize(500, 600);
         frame.getContentPane().setBackground(new Color(0, 0, 255, 20));
         frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        // add text area
+        JTextArea textarea = new JTextArea();
+        JScrollPane scrollpane = new JScrollPane(textarea, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        frame.add(scrollpane);
 
         // menubar
         JMenuBar menubar = new JMenuBar();
 
-        // menu 1
+        // menu 1 --> File
         JMenu menu1 = new JMenu("File");
         // menu items
         String[] menuitems1 = { "Create", "Open", "Save", "Save as" };
@@ -34,7 +41,7 @@ public class MenubarIntro {
         menu1.insertSeparator(2); // adds a separator between the menuitems
         menubar.add(menu1);
 
-        // menu 2
+        // menu 2 --> Edit
         JMenu menu2 = new JMenu("Edit");
         // menu items
         String[] menuitems2 = { "Cut", "Copy", "Paste" };
@@ -44,8 +51,19 @@ public class MenubarIntro {
 
         menubar.add(menu2);
 
-        JMenu menu3 = new JMenu("Help");
+        // menu 3 --> view
+        JMenu menu3 = new JMenu("View");
+        JMenuItem zoomitem = new JMenuItem("Zoom");
+        // Checkbox type menu item
+        JCheckBoxMenuItem checkboxitem = new JCheckBoxMenuItem("Show status bar", true);
+
+        menu3.add(zoomitem);
+        menu3.add(checkboxitem);
         menubar.add(menu3);
+
+        // menu 4 --> Help
+        JMenu menu4 = new JMenu("Help");
+        menubar.add(menu4);
 
         frame.setJMenuBar(menubar);
         frame.setVisible(true);
