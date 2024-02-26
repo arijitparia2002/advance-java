0ad577d9f2cf31ae760a149811dd28dd3797970f
Arijit Paria <arijitparia2020@gmail.com>
2024-02-26 10:29:53 +0530

    path: updates in path java

--- PathTest.java
+++ PathTest.java
@@ -9,5 +9,17 @@ public class PathTest {
         System.out.println(p1.getParent());
         System.out.println(p1.getFileName());
         System.out.println(p1.getName(0)); //0th index dosent count root dir
+
+
+        // normalize method to resolve the path
+        Path p2 = Paths.get("c:/abc/../pqr/./demo.txt"); //["." --> current dir | ".." --> parent dir]
+        System.out.println(p2.normalize());
+
+        //resolve method
+        System.out.println(p1.resolve(p2)); 
+        //if p2 is absolute thenn resolve will give oath of p2
+        Path p3 = Paths.get("pqr/../demo.txt"); // if p2 is relative append with p1
+        System.out.println(p1.resolve(p3));
+        System.out.println(p3.resolve(p1));
     }
 }
